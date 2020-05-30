0d525c5760c239986d6d462d7acda6f440bd827a
Stuart Schechter <stuart.schechter@gmail.com>
2020-05-30 15:34:13 +0900

    uncomment stuff that shouldn't have been commented (thanks CI!)

--- src/read-dickey.test.ts
+++ src/read-dickey.test.ts
@@ -12,11 +12,11 @@ test('Process dice image', async () => {
     //     const p = path.resolve( __dirname, pc.slice(0, i+1).join("/"));
     //     console.log(p, JSON.stringify(fs.readdirSync(p), null, 2));
     // });
-    // const image = await Jimp.read(
-    //     path.resolve(
-    //         __dirname,
-    //         '../cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg'
-    // ));
+    const image = await Jimp.read(
+        path.resolve(
+            __dirname,
+            '../cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg'
+    ));
     const diceKeyImageProcessor = new (await DiceKeyImageProcessorModulePromise).DiceKeyImageProcessor();
     const {bitmap} = image;
     const beforeMs = (new Date()).getTime();
