c66ff776fcf1be641f2a76743a132fbc78d346fe
Stuart Schechter <stuart.schechter@gmail.com>
2020-05-30 11:58:32 +0900

    move demo to dicekeys-js this becomes package for image processing only add CI add publish on release

--- src/dicekey-image-processor.ts
+++ src/dicekey-image-processor.ts
@@ -1,16 +1,41 @@
-import DiceKeyImageProcessorModuleNotReallyAPromiseFn from "read-dicekey-js";
-import {
-  DiceKeyImageProcessorModule,
-  PtrAllocatedInDiceKeyImageProcessorModule
-} from "read-dicekey-js";
+/// <reference types="emscripten" />
+const DiceKeyImageProcessorModuleNotReallyAPromiseFn =
+  require("./read-dicekey-js") as () => DiceKeyImageProcessorModuleNotReallyAPromise;
+
 import {
   getWebAsmModulePromiseWithAugmentedTypes,
   TypedMemoryHelpersForEmscriptenModule,
 } from "@dicekeys/webasm-module-memory-helper";
 
-export {
-  DiceKeyImageProcessor
-} from "read-dicekey-js";
+export enum MemoryAllocatedInDiceKeyImageProcessorModule { _ = 0 }
+export type PtrAllocatedInDiceKeyImageProcessorModule = number & MemoryAllocatedInDiceKeyImageProcessorModule;
+export type HasPtrAllocatedInDiceKeyImageProcessorModule = {byteOffset: PtrAllocatedInDiceKeyImageProcessorModule}
+
+export declare class DiceKeyImageProcessor {
+    constructor();
+    //   .function("processJsImageData", &DiceKeyImageProcessor::processJsImageData)
+    processImageData: (width: number, height: number, data: Uint8ClampedArray | Uint8Array | Buffer) => boolean;
+    //    .function("renderAugmentationOverlay", &DiceKeyImageProcessor::renderAugmentationOverlay)
+    renderAugmentationOverlay: (width: number, height: number, data: PtrAllocatedInDiceKeyImageProcessorModule) => boolean;
+    // .function("diceKeyReadJson", &DiceKeyImageProcessor::jsonKeySqrRead)
+    diceKeyReadJson: () => string;
+    // .function("isFinished", &DiceKeyImageProcessor::isFinished)  }
+    isFinished: () => boolean;
+    // All embind-generated objects are given a delete() method to destruct and clean up memory
+    delete: () => void;
+}
+
+export interface DiceKeyImageProcessorModule extends EmscriptenModule {
+    DiceKeyImageProcessor: new () => DiceKeyImageProcessor;
+}
+
+// Emscripten promises are kinda like promises... but it's important that we
+// not get lulled into thinking they're actually promises.
+// https://github.com/emscripten-core/emscripten/issues/5820
+interface DiceKeyImageProcessorModuleNotReallyAPromise {
+    then: (fn: (DiceKeyImageProcessor: DiceKeyImageProcessorModule) => any) => void
+}
+
 
 export type DiceKeyImageProcessorModuleWithHelpers = DiceKeyImageProcessorModule &
   TypedMemoryHelpersForEmscriptenModule<PtrAllocatedInDiceKeyImageProcessorModule>;
@@ -21,17 +46,3 @@ export type DiceKeyImageProcessorModuleWithHelpers = DiceKeyImageProcessorModule
  */
 export const DiceKeyImageProcessorModulePromise: Promise<DiceKeyImageProcessorModuleWithHelpers> =
   getWebAsmModulePromiseWithAugmentedTypes(DiceKeyImageProcessorModuleNotReallyAPromiseFn())
-
-
-
-// import{ BaseEmscriptenModule, addTsMemoryToModule } from "./typed-webasm-module-memory-helpers";
-// function neverRunThis_It_exists_purely_to_test_typings() {
-//   const testThisModulesTypes = undefined as DiceKeyImageProcessorModuleWithHelpers;
-//   enum SOME_OTHER_MODULE_PTR { _ = 0 };
-//   const otherModule = undefined as BaseEmscriptenModule;
-//   const augmentedOtherModule = addTsMemoryToModule<EmscriptenModule, SOME_OTHER_MODULE_PTR>(otherModule);
-
-//   augmentedOtherModule.tsMemory.usingByteArray(100, (bufferWithInvaildType) => {
-//     new testThisModulesTypes.DiceKeyImageProcessor().renderAugmentationOverlay(0, 0, bufferWithInvaildType.byteOffset);
-//   });
-// }
