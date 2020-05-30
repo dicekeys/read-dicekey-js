b2e22f4b8f6423862b451f67a01e27c45b83bded
Stuart Schechter <UppaJung@users.noreply.github.com>
2020-05-30 15:37:07 +0900

    Add Continuous Integration (CI) (#1)

--- src/read-dickey.test.ts
+++ src/read-dickey.test.ts
@@ -1,8 +1,22 @@
 import {DiceKeyImageProcessorModulePromise} from "./dicekey-image-processor"
+var path = require('path');
+import * as fs from "fs";
+
 import Jimp from "jimp"
 
 test('Process dice image', async () => {
-    const image = await Jimp.read('./cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg');
+    // Uncomment this if you're having problems with CI not finding the directory
+    // in the submodule and want to see the dir layout before the CI failure...
+    // const pc = ["..", "cpp", "read-dicekey", "tests", "test-lib-read-keysqr", "img"];
+    // [...Array(pc.length).keys()].forEach( (i) => {
+    //     const p = path.resolve( __dirname, pc.slice(0, i+1).join("/"));
+    //     console.log(p, JSON.stringify(fs.readdirSync(p), null, 2));
+    // });
+    const image = await Jimp.read(
+        path.resolve(
+            __dirname,
+            '../cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg'
+    ));
     const diceKeyImageProcessor = new (await DiceKeyImageProcessorModulePromise).DiceKeyImageProcessor();
     const {bitmap} = image;
     const beforeMs = (new Date()).getTime();
@@ -14,7 +28,11 @@ test('Process dice image', async () => {
 });
 
 test('Augment dice image', async () => {
-    const image = await Jimp.read('./cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg');
+    const image = await Jimp.read(
+        path.resolve(
+            __dirname,
+            '../cpp/read-dicekey/tests/test-lib-read-keysqr/img/B23X21K21Y63F53I50O11H12J40M13G10P40C60S33U23A21W62L60V42D33T32Z61N13E33R63.jpg'
+    ));
     const mod = await DiceKeyImageProcessorModulePromise;
     const diceKeyImageProcessor = new mod.DiceKeyImageProcessor();
     const {bitmap} = image;
