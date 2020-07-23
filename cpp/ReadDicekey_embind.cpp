4cc0d2f2b43747ecfa161ededfc0ee9fffe33747
Stuart Schechter <stuart.schechter@gmail.com>
2020-07-23 10:08:12 +0900

    add support for augmenting images

--- cpp/ReadDicekey_embind.cpp
+++ cpp/ReadDicekey_embind.cpp
@@ -44,12 +44,22 @@ inline void renderAugmentationOverlay(
 	thisDiceKeyImageProcessor.renderAugmentationOverlay(width, height, (uint32_t*) rgbaArrayPtr);
 };
 
+inline void augmentRGBAImage(	
+	DiceKeyImageProcessor& thisDiceKeyImageProcessor,
+	int width,
+	int height,
+	size_t rgbaArrayPtr
+) {
+	thisDiceKeyImageProcessor.augmentRGBAImage(width, height, (uint32_t*) rgbaArrayPtr);
+};
+
 
 EMSCRIPTEN_BINDINGS(DiceKeyImageProcessor) {
   class_<DiceKeyImageProcessor>("DiceKeyImageProcessor")
     .constructor()
     .function("processImageData", &processImageData)
     .function("renderAugmentationOverlay", &renderAugmentationOverlay) //, allow_raw_pointers())
+    .function("augmentRGBAImage", &augmentRGBAImage) //, allow_raw_pointers())
     .function("diceKeyReadJson", &DiceKeyImageProcessor::jsonKeySqrRead)
     .function("isFinished", &DiceKeyImageProcessor::isFinished)
     ;
