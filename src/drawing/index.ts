874aad9c3ed5d3f83f6fb5d31d1c172260e3e313
Stuart Schechter <stuart.schechter@gmail.com>
2020-09-24 13:21:31 +0900

    remove circular dependency

--- src/drawing/index.ts
+++ src/drawing/index.ts
@@ -1,6 +1,5 @@
 export * from "./create-angular-coordinate-system";
 export * from "./draw-rotated-and-scaled-image";
 export * from "./draw-rotated-rect";
-export * from "../render-faces-read";
 export * from "./line";
 export * from "./point";
